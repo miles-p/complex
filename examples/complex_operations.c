ea8ff05d7fb6506736787e6d0952523484bddb17
Miles Punch <71351127+miles-p@users.noreply.github.com>
2025-03-10 09:18:29 +1100

    new test

--- examples/complex_operations.c
+++ examples/complex_operations.c
@@ -2,10 +2,15 @@
 #include <stdio.h>
 
 int main() {
-    cartesianComplexNumber a = {1, 2};
+    cartesianComplexNumber z = {3, -4};
+    cartesianComplexNumber w = {1, 2};
     cartesianComplexNumber i = {0, 1};
-
-    multiplyComplexCartesian(a, i);
-    printf("The product of 1 + 2i and i is %f + %fi\n", multiplyComplexCartesian(a, i).real, multiplyComplexCartesian(a, i).imaginary);
-    return 0;
+    cartesianComplexNumber three = {3, 0};
+    int modz = cartesianComplexModulus(z);
+    cartesianComplexNumber compmodz = {modz, 0};
+    cartesianComplexNumber conjw = conjugateComplexCartesian(w);
+    cartesianComplexNumber conjwp3i = addComplexCartesian(conjw, multiplyComplexCartesian(three, i));
+    cartesianComplexNumber immodz = multiplyComplexCartesian(i, compmodz);
+    cartesianComplexNumber immodzmconkwp3i = multiplyComplexCartesian(immodz, conjwp3i);
+    printf("result = %f + %fi\n", immodzmconkwp3i.real, immodzmconkwp3i.imaginary);
 }
